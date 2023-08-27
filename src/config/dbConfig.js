025a386aa30104bf79414559889a798804e881cf
Havochalo <officialmail85@gmail.com>
2023-08-27 23:07:07 +1000

    db connection created

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 export const connectMongo = () => {
   try {
     const mongoUrl = process.env.MONGO_CLIENT;
+    mongoose.set("strictQuery", true);
     const connect = mongoose.connect(mongoUrl);
     connect && console.log("Mongo Db connection created");
   } catch (error) {
